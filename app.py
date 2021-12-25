0d28eb6cf5226a6620150abbcc7c1d3de8106179
ty0601 <tommy00660011@gmail.com>
2021-12-25 12:24:45 +0800

    Testing line bot reply

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-import re
 import sys
 
 from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 from fsm import TocMachine
 from utils import send_text_message
@@ -37,6 +36,7 @@ machine = TocMachine(
 
 app = Flask(__name__, static_url_path="")
 
+
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -66,20 +66,14 @@ def callback():
 
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
-        # if not isinstance(event, MessageEvent):
-        #     continue
-        # if not isinstance(event.message, TextMessage):
-        #     continue
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
 
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(TextMessage)
         )
-        # if re.Match('sticker', TextMessage):
-        #     sticker_message = StickerSendMessage(
-        #         package_id='1',
-        #         sticker_id='1'
-        #     )
-        #     line_bot_api.reply_message(event.reply_token, sticker_message)
 
     return "OK"
 
@@ -108,7 +102,7 @@ def webhook_handler():
         print(f"\nFSM STATE: {machine.state}")
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
-        if not response:
+        if response == False:
             send_text_message(event.reply_token, "Not Entering any State")
 
     return "OK"
