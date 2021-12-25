28c996a432ff57f8aa9f16b9b6e63fc29728453b
ty0601 <tommy00660011@gmail.com>
2021-12-26 03:24:53 +0800

    Update the State graph

--- app.py
+++ app.py
@@ -157,10 +157,11 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("Fsm.png", prog="dot", format="png")
+    machine.get_graph().draw("fsm.png", prog="dot", format="png")
     return send_file("fsm.png", mimetype="image/png")
 
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
     app.run(host="0.0.0.0", port=port, debug=True)
+
