2e11ec5638676ca11b9c444ddbf1875d76e23a21
ty0601 <tommy00660011@gmail.com>
2021-12-27 05:33:55 +0800

    Testing api

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ def get_all_coins_price():
         'convert': 'USD'
     }
     coin_json = requests.get(price_url, params=parameters, headers=headers).json()
-    coins = coin_json["data"]
+    coins = coin_json['data']
     data = []
     for row in coins:
         row_data = []
