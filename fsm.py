1f4e3aaa396194f0c6075df57f5b09516c4a870c
ty0601 <tommy00660011@gmail.com>
2021-12-27 07:48:03 +0800

    Testing api

--- fsm.py
+++ fsm.py
@@ -70,21 +70,23 @@ class TocMachine(GraphMachine):
     def on_enter_price(self, event):
         global curr_coin
         reply_token = event.reply_token
-        reply_message = FlexSendMessage("coin price", message_json.price_info)
+        # reply_message = FlexSendMessage("coin price", message_json.price_info)
+        buffer = message_json.price_info
         coin_price = get_coin_price(curr_coin)
         if not coin_price:
              send_text_message(reply_token, "Sorry, I can't find the coin")
         else:
-            reply_message['body']['contents'][0]['contents'][0]['text'] = coin_price[0] + ' - (' + coin_price[1] + ')'
-            reply_message['body']['contents'][1]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[2]
-            reply_message['body']['contents'][2]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[3]
-            reply_message['body']['contents'][3]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[4]
-            reply_message['body']['contents'][4]['contents'][1]['contents'][0]['text'] = coin_price[4] + '%'
-            reply_message['body']['contents'][5]['contents'][1]['contents'][0]['text'] = coin_price[5] + '%'
-            reply_message['body']['contents'][6]['contents'][1]['contents'][0]['text'] = coin_price[6] + '%'
-            reply_message['body']['contents'][7]['contents'][1]['contents'][0]['text'] = coin_price[7] + '%'
+
+            buffer['body']['contents'][0]['contents'][0]['text'] = coin_price[0] + ' - (' + coin_price[1] + ')'
+            buffer['body']['contents'][1]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[2]
+            buffer['body']['contents'][2]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[3]
+            buffer['body']['contents'][3]['contents'][1]['contents'][0]['text'] = '$ ' + coin_price[4]
+            buffer['body']['contents'][4]['contents'][1]['contents'][0]['text'] = coin_price[4] + '%'
+            buffer['body']['contents'][5]['contents'][1]['contents'][0]['text'] = coin_price[5] + '%'
+            buffer['body']['contents'][6]['contents'][1]['contents'][0]['text'] = coin_price[6] + '%'
+            buffer['body']['contents'][7]['contents'][1]['contents'][0]['text'] = coin_price[7] + '%'
         line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-        line_bot_api.reply_message(reply_token, reply_message)
+        line_bot_api.reply_message(reply_token, FlexSendMessage("coin price", buffer))
 
     def on_enter_metadata(self, event):
         reply_token = event.reply_token
