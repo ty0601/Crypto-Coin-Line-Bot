834ca9be56472fa453670559fda84ec1b1c79e82
ty0601 <tommy00660011@gmail.com>
2021-12-26 16:02:56 +0800

    Testing message_json.py of menu

--- message_json.py
+++ message_json.py
@@ -1,69 +1,86 @@
 menu = {
-    "type": "carousel",
-    "contents": [
-        {
-            "type": "bubble",
-            "hero": {
-                "type": "image",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNIVL73MsdTLq3_8Pmu76bsb8XfhblqQ3TFw&usqp=CAU"
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNIVL73MsdTLq3_8Pmu76bsb8XfhblqQ3TFw&usqp=CAU"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "lg",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Intorduction",
+              "uri": "https://linecorp.com"
             },
-            "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "lg",
-                "contents": [
-                    {
-                        "type": "button",
-                        "style": "primary",
-                        "action": {
-                            "type": "uri",
-                            "label": "Intorduction",
-                            "uri": "https://linecorp.com"
-                        },
-                        "color": "#22BB7C"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "bubble",
-            "hero": {
-                "type": "image",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "url": "https://image.shutterstock.com/image-vector/cryptocurrency-logo-collected-main-64-260nw-1910148376.jpg"
+            "color": "#22BB7C"
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": "https://image.shutterstock.com/image-vector/cryptocurrency-logo-collected-main-64-260nw-1910148376.jpg"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "lg",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Crypto Coins",
+              "uri": "https://linecorp.comhttps://linecorp.comhttps://linecorp.com"
             },
-            "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "lg",
-                "contents": [
-                    {
-                        "type": "button",
-                        "style": "primary",
-                        "action": {
-                            "type": "uri",
-                            "label": "Crypto Coins",
-                            "uri": "https://linecorp.comhttps://linecorp.comhttps://linecorp.com"
-                        },
-                        "color": "#FEC72D"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "bubble",
-            "hero": {
-                "type": "image",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "url": "https://github.com/ty0601/LINE-BOT/blob/master/fsm.png?raw=true"
-            }
-        }
-    ]
+            "color": "#FEC72D"
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHfFXkhqh3qFENwyU1W9lxPJwcEtaft1ogZg&usqp=CAU",
+        "aspectMode": "fit"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "lg",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "FSM Graph",
+              "uri": "https://linecorp.comhttps://linecorp.comhttps://linecorp.com"
+            },
+            "color": "#808080"
+          }
+        ]
+      }
+    }
+  ]
 }
