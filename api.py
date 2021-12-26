232223a3f5f219d85d01b48434a7f0a731cd1bed
ty0601 <tommy00660011@gmail.com>
2021-12-27 04:50:32 +0800

    Testing api

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import os
 
@@ -15,20 +17,20 @@ def get_all_coins_price():
         'limit': '100',
         'convert': 'USD'
     }
-    coin_json = requests.get(price_url, params=parameters, headers=headers).json()
-    coins = coin_json
+    coin_json = requests.get(price_url, params=parameters, headers=headers)
+    coins = json.loads(coin_json['data'])
     data = []
     for row in coins:
         row_data = []
-        row_data.append(row['data']['symbol'])
-        row_data.append(row['data']['name'])
-        row_data.append(round(row['data']['quote']['USD']['price'], 2))
-        row_data.append(round(row['data']['quote']['USD']['market_cap']))
-        row_data.append(round(row['data']['quote']['USD']['volume_24h']))
-        row_data.append(round(row['data']['quote']['USD']['percent_change_1h'], 4))
-        row_data.append(round(row['data']['quote']['USD']['percent_change_24h'], 4))
-        row_data.append(round(row['data']['quote']['USD']['percent_change_7d'], 4))
-        row_data.append(round(row['data']['quote']['USD']['percent_change_30d'], 4))
+        row_data.append(row['name'])
+        row_data.append(row['symbol'])
+        row_data.append(round(row['quote']['USD']['price'], 2))
+        row_data.append(round(row['quote']['USD']['market_cap']))
+        row_data.append(round(row['quote']['USD']['volume_24h']))
+        row_data.append(round(row['quote']['USD']['percent_change_1h'], 4))
+        row_data.append(round(row['quote']['USD']['percent_change_24h'], 4))
+        row_data.append(round(row['quote']['USD']['percent_change_7d'], 4))
+        row_data.append(round(row['quote']['USD']['percent_change_30d'], 4))
         data.append(row_data)
     return data
 
