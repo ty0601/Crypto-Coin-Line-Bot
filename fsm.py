4df11d4c3825a1422f58056379779208041d60e5
ty0601 <tommy00660011@gmail.com>
2021-12-29 06:28:43 +0800

    Update code

--- fsm.py
+++ fsm.py
@@ -8,7 +8,7 @@ from api import get_coin_price, get_coin_metadata
 
 
 class TocMachine(GraphMachine):
-    curr_coin = []
+    curr_coin = ''
 
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
@@ -19,9 +19,7 @@ class TocMachine(GraphMachine):
 
     def is_going_to_coin_menu(self, event):
         global curr_coin
-        if not curr_coin[event.source.user_id]:
-            curr_coin[event.source.user_id] = ''
-        curr_coin[event.source.user_id] = event.message.text
+        curr_coin = event.message.text
         return True
 
     def is_going_to_choose_coins(self, event):
@@ -56,8 +54,7 @@ class TocMachine(GraphMachine):
 
     def on_enter_choose_coins(self, event):
         global curr_coin
-        print("enter choose coins" + curr_coin[event.source.user_id])
-        curr_coin[event.source.user_id] = ''
+        curr_coin= ''
         reply_token = event.reply_token
         reply_message = FlexSendMessage("choose coin", message_json.choose_coin)
         line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
@@ -72,7 +69,7 @@ class TocMachine(GraphMachine):
     def on_enter_price(self, event):
         global curr_coin
         reply_token = event.reply_token
-        coin_price = get_coin_price(curr_coin[event.source.user_id])
+        coin_price = get_coin_price(curr_coin)
         if not coin_price:
             reply_message = FlexSendMessage("not found", message_json.not_found_coin_menu)
             line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
@@ -94,7 +91,7 @@ class TocMachine(GraphMachine):
     def on_enter_metadata(self, event):
         global curr_coin
         reply_token = event.reply_token
-        coin_data = get_coin_metadata(curr_coin[event.source.user_id])
+        coin_data = get_coin_metadata(curr_coin)
         if not coin_data:
             reply_message = FlexSendMessage("not found", message_json.not_found_coin_menu)
             line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
